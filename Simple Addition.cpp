04c11c565fae49cc1eedd6707bba09cc3645f478
Hemant9143 <40552777+Hemant9143@users.noreply.github.com>
2018-06-25 07:45:06 +0530

    It is a programe of simple addition

---
+++ Simple Addition.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a,b;
+   cout<<"Enter Frist Number =";
+   cin>>a;
+   cout<<"Enter Second Noumber =";
+   cin>>b;
+   int z;
+   z = a+b;
+//   cout<<"z"<<endl;                  // z  will be answer of this.
+   cout<<z<<endl;                   // 300 will be answer of this.
+//   cout<<"sum of 100 and 200 is 300";
+//   cout<<"Sum of"<<a<<"and"<<b<<"is"<<z<<endl;
+//         300 is the sum of 100 and 200;
+//   cout<<z<<"is the sum of"<<z<<"and"<<b<<endl;
+//         100+200=300;
+//   cout<<a<<"\t"<<b<<"="<<z<<endl;
+     return(0);
+}
